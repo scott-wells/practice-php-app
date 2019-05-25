1660cee5cc2d73b172999b272bd90c12c6bcba76
Scott Wells <efwells101@gmail.com>
2019-05-25 15:08:58 -0600

    changed file names, include on server

--- header.php
+++ header.php
@@ -2,16 +2,17 @@
     session_start();
 
 echo <<<_INIT
-<!DOCTYPE html>
+    
+    <!DOCTYPE html>
     <html>
-        <head>
-            <meta charset='utf-8'>
-            <meta name='viewport' conent='width=device-width, initial-scale=1'>
-            <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css'>
-            <link rel='stylesheet' href='style.css'>
-            <script src='javascript.js'></script>
-            <script src='jquery-2.2.4.min.js'></script>
-            <script src='jquery.mobile-1.4.5.min.js'></script>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' conent='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css'>
+        <link rel='stylesheet' href='style.css'>
+        <script src='javascript.js'></script>
+        <script src='jquery-2.2.4.min.js'></script>
+        <script src='jquery.mobile-1.4.5.min.js'></script>
 
 _INIT;
 
@@ -27,45 +28,48 @@ _INIT;
     else $loggedin = FALSE;
 
 echo <<<_MAIN
+        
         <title>Nerd Social: $userstr</title>
     </head>
 
     <body>
         <div data-role='page'>
             <div data-role='header'>
-                <div id='logo' class='center'>
-                    <img id='nerd-logo' src='logo.jpg'>
-                </div>
+                <div id='logo' class='center'><img id='nerd-logo' src='logo.jpg'></div>
                 <div class='username'>$userstr</div>
             </div>
             <div data-role='content'>
 
 _MAIN;
 
-    if($loggedin) 
-    {
-        echo <<<_LOGGEDIN
-            <div class='center'>
-                <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href='members.php?view=$user'>Home</a>
-                <a data-role='button' data-inline='true' data-transition="slide" href='members.php'>Members</a>
-                <a data-role='button' data-inline='true' data-transition="slide" href='friends.php'>Friends</a>
-                <a data-role='button' data-inline='true' data-transition="slide" href='messages.php'>Messages</a>
-                <a data-role='button' data-inline='true' data-transition="slide" href='profile.php'>Edit Profile</a>
-                <a data-role='button' data-inline='true' data-transition="slide" href='logout.php'>Log Out</a>
-            </div>
+    if($loggedin) {
+    
+echo <<<_LOGGEDIN
+        
+        <div class='center'>
+            <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href='members.php?view=$user'>Home</a>
+            <a data-role='button' data-inline='true' data-transition="slide" href='members.php'>Members</a>
+            <a data-role='button' data-inline='true' data-transition="slide" href='friends.php'>Friends</a>
+            <a data-role='button' data-inline='true' data-transition="slide" href='messages.php'>Messages</a>
+            <a data-role='button' data-inline='true' data-transition="slide" href='profile.php'>Edit Profile</a>
+            <a data-role='button' data-inline='true' data-transition="slide" href='logout.php'>Log Out</a>
+        </div>
 
-        _LOGGEDIN;
+_LOGGEDIN;
+        
+    } else {
+
+echo <<<_GUEST
+        
+        <div class='center'>
+            <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href=index.php'>Home</a>
+            <a data-role='button' data-inline='true' data-icon='plus'data-transition="slide" href='signup.php'>Sign Up</a>
+            <a data-role='button' data-inline='true' data-icon='check'data-transition="slide" href='login.php'>Log In</a>
+        </div>
+        <p class='info'>(You must be logged in to use this app.)</p>
+    
+_GUEST;
+
     }
-    else
-    {
-        echo <<<_GUEST
-            <div class='center'>
-                <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href=index.php'>Home</a>
-                <a data-role='button' data-inline='true' data-icon='plus'data-transition="slide" href='signup.php'>Sign Up</a>
-                <a data-role='button' data-inline='true' data-icon='check'data-transition="slide" href='login.php'>Log In</a>
-            </div>
-            <p class='info'>(You must be logged in to use this app.)</p>
-       
-         _GUEST;
-    }
+
 ?>
