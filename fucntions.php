f5de75aeda1533f9e6a9a7b1f89f43f6c5a3b958
Scott Wells <efwells101@gmail.com>
2019-05-25 14:18:09 -0600

    add destroySession function, santizeString function, showProfile function

--- fucntions.php
+++ fucntions.php
@@ -20,5 +20,32 @@
     }
 
     function destroySession() {
-        
+        $_SESSION = array();
+
+        if(session_id() != "" || isset($_COOKIE[session_name()]))
+            setcookie(session_name(), '', time()-2592000, '/');
+
+        session_destroy();
     }
+
+    function sanitizeString($var) {
+        global $connection;
+        $var = strip_tags($var);
+        $var = htmlentities($var);
+        if(get_magic_quotes_gpc())
+            $var = stripslashes($var);
+        return $connection->real_escape_string($var);
+    }
+
+    function showProfile($user) {
+        if(file_exists("$user.jpg")) // change this file name
+            echo "<img src='$user.jpg' style='float:left;'>";
+
+        $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+        if($result->num_rows) {
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+        } else echo "<p>Nothing to see here, yet.</p><br>";
+    }
+?>
