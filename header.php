e40accdb4d5bd2e01ea44987815265784eb8f520
Scott Wells <efwells101@gmail.com>
2019-05-25 14:55:44 -0600

    add page title, add body with logo, add logged in and guest menus

--- header.php
+++ header.php
@@ -25,3 +25,47 @@ _INIT;
         $userstr  = "Logged in as: $user";
     }
     else $loggedin = FALSE;
+
+echo <<<_MAIN
+        <title>Nerd Social: $userstr</title>
+    </head>
+
+    <body>
+        <div data-role='page'>
+            <div data-role='header'>
+                <div id='logo' class='center'>
+                    <img id='nerd-logo' src='logo.jpg'>
+                </div>
+                <div class='username'>$userstr</div>
+            </div>
+            <div data-role='content'>
+
+_MAIN;
+
+    if($loggedin) 
+    {
+        echo <<<_LOGGEDIN
+            <div class='center'>
+                <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href='members.php?view=$user'>Home</a>
+                <a data-role='button' data-inline='true' data-transition="slide" href='members.php'>Members</a>
+                <a data-role='button' data-inline='true' data-transition="slide" href='friends.php'>Friends</a>
+                <a data-role='button' data-inline='true' data-transition="slide" href='messages.php'>Messages</a>
+                <a data-role='button' data-inline='true' data-transition="slide" href='profile.php'>Edit Profile</a>
+                <a data-role='button' data-inline='true' data-transition="slide" href='logout.php'>Log Out</a>
+            </div>
+
+        _LOGGEDIN;
+    }
+    else
+    {
+        echo <<<_GUEST
+            <div class='center'>
+                <a data-role='button' data-inline='true' data-icon='home' data-transition="slide" href=index.php'>Home</a>
+                <a data-role='button' data-inline='true' data-icon='plus'data-transition="slide" href='signup.php'>Sign Up</a>
+                <a data-role='button' data-inline='true' data-icon='check'data-transition="slide" href='login.php'>Log In</a>
+            </div>
+            <p class='info'>(You must be logged in to use this app.)</p>
+       
+         _GUEST;
+    }
+?>
